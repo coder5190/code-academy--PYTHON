f52316e34faf6829271a2a9484b09cce14d78645
vijikswamy <41676564+vijikswamy@users.noreply.github.com>
2018-09-05 07:48:36 +0530

    Add files via upload

---
+++ Difference btw python 2 and python 3.py
@@ -0,0 +1,1 @@
+print ("There are two different Python versions. Both Python 2 and Python 3 are used throughout the globe. The most significant difference between the two is how you write a print statement. In Python 3, print has parentheses.")
