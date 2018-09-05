c7e609c88d1bf29fb8113e357c80f1ec6fc6c332
vijikswamy <41676564+vijikswamy@users.noreply.github.com>
2018-09-05 08:19:33 +0530

    Rename Difference btw python 2 and python 3.py to 2.Difference btw python 2 and python 3.py

--- Difference btw python 2 and python 3.py
+++
@@ -1,1 +0,0 @@
-print ("There are two different Python versions. Both Python 2 and Python 3 are used throughout the globe. The most significant difference between the two is how you write a print statement. In Python 3, print has parentheses.")
